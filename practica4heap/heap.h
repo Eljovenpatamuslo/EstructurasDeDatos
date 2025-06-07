4c55731d9bd5a57e617ecfe89fc62c337661eed6
caffre <jaffreagustin18205@gmail.com>
2025-06-07 16:17:16 -0300

    Heap terminado

--- practica4heap/heap.h
+++ practica4heap/heap.h
@@ -1,12 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int (*FuncionComparadora)(const void *dato1,const void *dato2);
+#ifndef HEAP_H
+#define HEAP_H
+
+typedef int (*FuncionComparadora)(void *dato1, void *dato2);
 typedef void (*FuncionDestructora)(void *dato);
 typedef void *(*FuncionCopia)(void *dato);
 typedef void (*FuncionVisitante)(void *dato);
 
-typedef struct _BHeap {
+
+typedef struct _BHeap{
+    void **arr;
+    int capacidad;
+    int ultimo;
+    FuncionComparadora comp;
+    FuncionCopia copy;
+    FuncionDestructora destr;
+ } *BHeap;
+
+
+// Crea un BHeap binario vacío
+BHeap bheap_crear(FuncionComparadora comp, FuncionCopia copy, FuncionDestructora destr);
+
+// Determina si el BHeap es vacio
+int bheap_es_vacio(BHeap heap);
+
+// Inserta un elemento en un BHeap
+void bheap_insertar(void* data, BHeap heap);
+
+// Retorna y elimina el elemento en el tope del BHeap
+void* bheap_pop(BHeap heap);
+
+// Recorre el BHeap
+void bheap_recorrer(FuncionVisitante visit, BHeap heap);
+
+// Elimina un elemento dado del BHeap
+void bheap_eliminar(void* dato, BHeap heap);
+
+// Destruye un BHeap
+void bheap_destruir(BHeap heap);
+
+// Crea un BHeap desde un array.
+// No crea una copia del array, utiliza la misma referencia.
+BHeap bheap_crear_desde_arr(void **arr, int largo,FuncionComparadora comp, FuncionCopia copiar, FuncionDestructora destr);
+
+// Heapsort
+void heapSort(void** arr, int largo, FuncionComparadora comp);
+
+/*typedef struct _BHeap {
     void **arr;
     int capacidad;
     int ultimo;
@@ -20,4 +62,5 @@ void bheap_recorrer(BHeap raiz, FuncionVisitante visit);
 BHeap bheap_insertar(BHeap raiz, void* dato);
 BHeap bheap_eliminar(BHeap raiz, void* dato);
 
-BHeap bheap_crear_desde_arr(void **arr, int largo, FuncionCopia copiar, FuncionComparadora comp);
+BHeap bheap_crear_desde_arr(void **arr, int largo, FuncionCopia copiar, FuncionComparadora comp);*/
+#endif
--- practica4heap/test.c
+++ practica4heap/test.c
@@ -1,54 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
 #include "heap.h"
-
-int comparadora(const void* dato1,const void* dato2){
-    int data1 = *((int*)dato1);
-    int data2 = *((int*)dato2);
-    if(data1 > data2) return 1;
-    if(data1 < data2) return -1;
-    else return 0;
+void print_int(void* n){
+    printf("%d\n",*(int*)n);
 }
 
-int comparadora_evil(const void* dato1,const void* dato2){
-    int data1 = *((int*)dato1);
-    int data2 = *((int*)dato2);
-    if(data1 < data2) return 1;
-    if(data1 > data2) return -1;
-    else return 0;
+void* copy_int(void* n){
+    return n;
 }
 
-
-void imprimir_entero(void* dato){
-    printf("%i",*((int*)dato));
-} 
-
-static void *copiar_int(void* dato){
-  int *copia = malloc(sizeof(int));
-  *copia = *((int *) dato);
-  return copia; 
+void destr_int(void* n){
+    (void) n;
+    return;
 }
 
-int main(){
-    int dato;
-    dato = 1;
-    BHeap arbol = bheap_crear(10,comparadora);
-    arbol = bheap_insertar(arbol, &dato);
-    int dato1 = 2;
-    arbol = bheap_insertar(arbol, &dato1);
-    int dato2 = 3;
-    arbol = bheap_insertar(arbol, &dato2);
-    int dato3 = 4;
-    arbol = bheap_insertar(arbol, &dato3);
-    int dato4 = 5;
-    arbol = bheap_insertar(arbol, &dato4);
+int comp_int(void* n1, void* n2){
+    int _n1 = *(int*)n1;
+    int _n2 = *(int*)n2;
+    return _n1 - _n2;
+}
 
-    //arbol = bheap_eliminar(arbol,&dato3);
-    void* arr[4];
-    arr[0] = &dato;
-    arr[1] = &dato1;
-    arr[2] = &dato2;
-    arr[3] = &dato3;
-    bheap_recorrer(arbol,imprimir_entero);
-    BHeap arbol2 = bheap_crear_desde_arr(arr,4,copiar_int,comparadora_evil);
-    bheap_recorrer(arbol2,imprimir_entero);
+void print_arr(void** arr, int largo) {
+    for (int i = 0; i < largo; i++) {
+        print_int(arr[i]);
+    }
+    printf("\n");
+}
+
+void test_bheap(){
+    // Crear el heap
+    BHeap heap = bheap_crear(comp_int, copy_int, destr_int);
+
+    // Insertar 5 elementos
+    int valores[] = {8, 42, 23, 15, 16, 5, 13};
+    for (int i = 0; i < 7; i++) {
+        bheap_insertar(&valores[i], heap);
+    }
+    
+    printf("Recorrido del heap:\n");
+    bheap_recorrer(print_int, heap);
+    printf("\n");
+    
+    printf("Extrayendo elementos del heap (en orden):\n");
+    while (!bheap_es_vacio(heap)) {
+        int* max = (int*)bheap_pop(heap);
+        printf("%d ", *max);
+    }
+    printf("\n");
+
+    // Destruir heap
+    bheap_destruir(heap);
+}
+
+void test_heapsort(int n){
+    void** arr = malloc(sizeof(void*) * n);
+    assert(arr);
+
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = malloc(sizeof(int));
+        *(int*)arr[i] = rand() % (n + 1); 
+    }
+    
+    printf("Arreglo original:\n");
+    print_arr(arr, n);
+
+    heapSort(arr, n, comp_int);
+
+    printf("Arreglo ordenado con heapSort:\n");
+    print_arr(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+}
+
+int main(int argv, char* argc[]) {
+
+    test_bheap();
+    int test_size = atoi(argc[1]); 
+    test_heapsort(test_size);
     return 0;
 }
