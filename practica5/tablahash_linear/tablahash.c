7d1c9cca56b99f67737f454f5a13e306807d778a
caffre <jaffreagustin18205@gmail.com>
2025-08-18 11:20:02 -0300

    tabla hash linear probing bien hecha

--- practica5/tablahash_linear/tablahash.c
+++ practica5/tablahash_linear/tablahash.c
@@ -62,35 +62,37 @@ void tablahash_destruir(TablaHash tabla) {
 
 void tablahash_insertar(TablaHash tabla, void *dato) {
 
-  // Calculamos la posicion del dato dado, de acuerdo a la funcion hash.
   unsigned idx = tabla->hash(dato) % tabla->capacidad;
-  // Insertar el dato si la casilla estaba libre.
-  if (tabla->elems[idx].ocupado == 0) {
+  int idx_ocupado = -1;
+
+  for(;tabla->elems[idx].dato != NULL && tabla->comp(tabla->elems[idx].dato,dato) != 0 ; idx = (idx+1) % tabla->capacidad){
+    if(tabla->elems[idx].ocupado == 0 && idx_ocupado == -1){
+      idx_ocupado = (int)idx;
+    }
+  }
+  if(tabla->elems[idx].dato != NULL){//encontró un dato idéntico, sobreescribir
+    tabla->destr(tabla->elems[idx].dato);
+    tabla->elems[idx].dato = tabla->copia(dato);
+    if(tabla->elems[idx].ocupado == 0){
+      tabla->numElems++;
+      tabla->elems[idx].ocupado = 1;
+    }
+  }
+  else if(idx_ocupado == -1){ //se detuvo en NULL y idx_ocupado == -1, no hay valores eliminados, escribir en donde estoy
     tabla->numElems++;
     tabla->elems[idx].dato = tabla->copia(dato);
     tabla->elems[idx].ocupado = 1;
   }
-  // Si en el lugar está si mismo, se reemplaza
-  else if (tabla->elems[idx].ocupado && tabla->comp(tabla->elems[idx].dato,dato) == 0){
-    tabla->destr(tabla->elems[idx].dato);
-    tabla->elems[idx].dato = tabla->copia(dato);
+  else{//se detuvo en NULL y idx_ocupado != -1 , hay valores eliminados, inserto en el primero de ellos
+    tabla->destr(tabla->elems[idx_ocupado].dato);
+    tabla->numElems++;
+    tabla->elems[idx_ocupado].dato = tabla->copia(dato);
+    tabla->elems[idx_ocupado].ocupado = 1;
   }
-   // Si el lugar ya está ocupado, recorremos la tabla hasta encontrar una posicion vacía
-  else{
-    
-    unsigned h = idx;
-    for(;tabla->elems[h].ocupado == 1;h = (h+1)%tabla->capacidad);
-    if (tabla->elems[h].ocupado == 0){
-        if (tabla->elems[h].dato != NULL)
-          tabla->destr(tabla->elems[h].dato);
-        tabla->elems[h].dato = tabla->copia(dato);
-        tabla->elems[h].ocupado = 1;
-        tabla->numElems++;
-    }
-  }
+
   float factor_de_carga = (float)tabla->numElems / (float)tabla->capacidad;
-  if (factor_de_carga >= 0.7)
-    tablahash_redimensionar(tabla);
+    if (factor_de_carga >= 0.7)
+      tablahash_redimensionar(tabla);
 }
 
 
@@ -104,17 +106,17 @@ void *tablahash_buscar(TablaHash tabla, void *dato) {
     unsigned idx = tabla->hash(dato) % tabla->capacidad;
 
   // Retornar NULL si la casilla estaba vacia.
-    if (tabla->elems[idx].ocupado == 0)
+    if (tabla->elems[idx].dato == NULL)
         return NULL;
     // Si la posicion está ocupada, vamos a recorrer la tabla hasta encontrarlo (o no!)
     else{
         unsigned h = idx;
         for(;tabla->elems[h].dato != NULL && tabla->comp(tabla->elems[h].dato,dato) != 0;h = (h+1)%tabla->capacidad);
         
-        if (tabla->elems[h].dato == NULL)
-          return NULL;
-        else{
+        if (tabla->elems[h].dato != NULL && tabla->elems[h].ocupado == 1)
           return tabla->elems[h].dato;
+        else{
+          return NULL;
         }
     }
 }
@@ -141,10 +143,16 @@ void tablahash_eliminar(TablaHash tabla, void *dato) {
         if (tabla->elems[h].dato == NULL)
           return;
         else{
-          tabla->elems[h].ocupado = 0;
-          tabla->numElems--;
+          if(tabla->elems[h].ocupado == 1){
+            tabla->elems[h].ocupado = 0;
+            tabla->numElems--;
+          }
+          return;
         }
   }
+}
+void* copydummy (void* dato){
+  return dato;
 }
 
 void tablahash_redimensionar(TablaHash tabla){
@@ -162,12 +170,15 @@ void tablahash_redimensionar(TablaHash tabla){
     tabla->elems[i].dato = NULL;
     tabla->elems[i].ocupado = 0;
   }
+  FuncionCopiadora buenaCopia = tabla->copia;
+  tabla->copia = copydummy;
 
   for (int i = 0; i < vieja_capacidad;i++){
     if (vieja_tabla[i].dato != NULL){
       tablahash_insertar(tabla,vieja_tabla[i].dato);
-      tabla->destr(vieja_tabla[i].dato);
       }
   }
+
+  tabla->copia = buenaCopia;
   free(vieja_tabla);
 }
